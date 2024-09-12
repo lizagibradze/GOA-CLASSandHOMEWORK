8f63fd47d1105f9dbce0bf80d4d64eb12b247920
lizagibradze <oreogibradze@gmail.com>
2024-09-13 02:40:04 +0400

    second commit

---
+++ class 2/level 3/classwork/task01.py
@@ -0,0 +1,14 @@
+name = "liza"
+
+last_name = "gibradze"
+
+print (name + ' ' + last_name)
+
+
+
+num1 = 100
+num2 = 50
+
+print (num1 - num2)
+
+(vscode-pets.start)
---
+++ class 2/level 3/homework/homework .py
@@ -0,0 +1,56 @@
+#i want to learn how to code so i joined goa 
+
+one  = " i"
+two  ="want "
+three = "to"
+four = "learn"
+five = "how"
+six = "to"
+seven  ="code"
+eight  = "so"
+nine = "i"
+ten = "joined"
+eleven = "goa"
+
+print ( one + " " + two + " "+ three + " " + four + " " + five + " " + six + " "  + seven + " " + eight + " " + nine + " "  +   ten + " " + eleven )
+
+
+name = "liza "
+last_name = "gibradze"
+age = "15"
+hobby = "drawing"
+country = "georgia "
+
+#ეხლა ჩემი სახელი გვარი ასაკი ჰობი და ქვეყანა უნდა დავპრინტოთ 
+print (name + " " + last_name + " "+ age + " " + hobby + " " + country )
+
+
+num0 = 2
+num1 = 100
+num2 = 50
+num3 = 10 
+num4 = 5
+num5 = 250
+num6 = 200
+
+
+print (num1 - num2) 
+print(num3 + num4)
+print (num2 * num1)
+print (num6 / num4)
+print (num4 // num3)
+print (num4 ** num0)
+print (num5 + num3 - num1)
+print (num6 * num0 / num4)
+print (num1 ** num0 // num4)
+print ( num2 + num3 - num3)
+
+
+
+a = 10
+b = 20
+
+ab = 30
+
+print (ab)
+
