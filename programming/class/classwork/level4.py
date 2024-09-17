cfbf08e3e3bf08a85682507566ada1165869b948
lizagibradze <oreogibradze@gmail.com>
2024-09-17 22:02:39 +0400

    second commit

---
+++ programming/class/classwork/level4.py
@@ -0,0 +1,72 @@
+# num1 =10
+
+# num2 = 5
+
+# sum= 15 #num1+num2 
+
+
+# print(sum)
+
+
+# price= 50
+# price= 60
+# print(price)
+
+
+
+
+
+
+
+
+
+
+
+# #debugging არის კოდის შესწორება
+
+# #Exercise 1:
+# print("Hello, World!")
+
+# #Exercise 2: 
+# greeting = "Good Morning"
+# print(greeting)
+
+# #Exercise 3: 
+# first_name = "Alice"
+# last_name = "Smith"
+# print( first_name + last_name)
+
+# #Exercise 4:
+# number = 50
+# print("The number is" + number)
+
+# #Exercise 5: 
+# name = "George"
+# print("Hello" + "name!")
+
+# #Exercise 6: 
+# age = 25
+# print("I am"+ age + "years old.")
+
+# #Exercise 7: 
+# num1 = "10"
+# num2 = "20"
+# result = num1 + num2
+# print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ programming/class/homework/level4 homework .py
@@ -0,0 +1,41 @@
+# 1
+
+
+a = 5
+b = 10
+
+
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a/b)
+
+# 2
+# გავაკეთე ორი ცვლადი . და ეხა პრინტ დაწერს წინადადებას
+
+name = "ლიზა"
+age = 15
+
+
+ 
+print("ჩემი სახელია" + " " + name + " " + " და ვარ " + str(age) + " " + "წლის")
+ 
+
+# 3
+# გავაკეთე ფოინთი და დავაუფდეითე . გამოვაკელი 5
+points = 50
+points = points -5
+print(points)
+
+# 4
+#ორი ცვლადი გავაკეთე
+
+age = 15
+birthyear = 2009
+current_year = 2024
+
+print(age)
+
+
