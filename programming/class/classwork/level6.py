958047ea49bad4eb7d16b7383dbe3026ac4058e9
lizagibradze <oreogibradze@gmail.com>
2024-09-25 00:02:50 +0400

    fourth commit

---
+++ programming/class/classwork/level6.py
@@ -0,0 +1,25 @@
+# name = input("რა არის შენი სახელი: ")
+
+
+# print("შენი სახელია", name)
+
+
+# surname = input("რა არის შენი გვარი: ")
+
+# print("შენი გვარია", surname)
+
+
+
+
+# print("შენი სახელი და გვარია: " + name+surname)
+
+num1 = int( input("Enter number1: "))
+num2 = int( input("Enter number2: "))
+
+print(num1 + num2)
+
+
+
+
+
+
