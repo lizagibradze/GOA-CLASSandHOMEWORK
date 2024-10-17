a71920247accdf5fc11c6ed2ae88348b611a1d2a
lizagibradze <oreogibradze@gmail.com>
2024-10-17 19:37:53 +0400

    13commit

---
+++ day12/classwork/level12.py
@@ -0,0 +1,66 @@
+# for i in range(0, 10000,2):
+#     print(i)
+
+# for i in range(1,10000,2):
+#         print(i)
+
+
+
+# while True:
+#     print("umar")
+
+
+
+# i = 0
+# while i < 10:
+#     print("umar")
+
+
+# i = 0
+# while i < 10:
+#     print("umar", i)
+
+#     # i = i + 1 itst he same as i +=1
+
+#     i += 1
+
+
+
+
+# i = 0  
+
+# while i < 4: 
+#     print('* * * * *')  
+#     i += 1  
+
+
+
+# i = 1 
+
+# i = 1
+# while i <= 10000:
+#     if i % 2 == 0:
+#         print(i)
+#     i += 1
+
+
+
+#     i = 1
+# while i <= 10000:
+#     if i % 2 != 0:
+#         print(i)
+#     i += 1
+
+
+
+user_password = input("enter password: ")
+password = "123"
+
+while user_password != password:
+    user_password = input("please try again ")
+print("login succsesful")
+
+
+
+
+
---
+++ day12/homework/level12.py
@@ -0,0 +1,46 @@
+    
+
+
+# 1
+
+for i in range(1, 50 , 2):
+    print(i)
+
+
+#2
+
+
+for i in range(4):
+    print("*" * 10)
+
+
+
+
+# 3
+
+for i in range(4):
+   print("*" * 5)
+
+
+
+# 4
+
+
+for num1 in range(1, 4):
+    for num in range(1, 4):
+        print(num1, num)
+
+
+
+# 5
+
+
+password = input("enter password: ")
+password = "liza"
+
+while password != password:
+    password = input("please try again ")
+print("login succsesful")
+
+
+
