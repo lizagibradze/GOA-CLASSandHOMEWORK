d3fdd466186e2cf45dc4440ed134bd897a9f7c96
lizagibradze <oreogibradze@gmail.com>
2024-10-20 22:36:43 +0400

    14commit

---
+++ day13/homework/level13.py
@@ -0,0 +1,22 @@
+# 1
+for i in "name":
+    print(i)
+
+
+#  name არის 4 ასო . for თითო ასოს სათითაოდ კითხულობს და პრინტავს
+
+# n
+# a  
+# m 
+# e 
+
+# 2
+
+
+age = int(input("how old are you? "))
+
+if age>10:
+    print("you are more than 10 years old")
+
+else:
+    print(False)
