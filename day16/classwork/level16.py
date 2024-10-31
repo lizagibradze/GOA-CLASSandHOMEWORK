5fe1fe16341599b61605ffc4d06d406087c2892f
lizagibradze <oreogibradze@gmail.com>
2024-10-31 20:45:47 +0400

    16commit

---
+++ day16/classwork/level16.py
@@ -0,0 +1,48 @@
+
+# user = input(" შეიყვანეთ ცხვარი:")
+
+# while user != "გოჭს":
+#     user = input("შეიყვანეთ გოჭი")
+#     print("ცხვარი გოჭია")
+# print( "ცხვარი გოჭი არ არის")
+
+
+
+
+
+
+            # 0        1       2          3      4   
+# array = ["daviti", "giorgi", "tsotne", 12 , True]
+# array[0] = "tkac"
+
+# print(array[4])
+# print (array)
+
+
+
+
+# array = ["food" , "drink" , "shoes" , "pants"]
+
+# print(array[0] ,array[1] , array[2] ,array[3])
+
+
+# print(array[1])
+# print(array[2])
+# print(array[3])
+
+
+
+
+
+array = ["daviti", "giorgi", "tsotne", 12 , True]
+array[0] = "tkac"
+
+print(array[4])
+print("daviti")
+
+
+name = "group05"
+
+print(name[1])
+
+
