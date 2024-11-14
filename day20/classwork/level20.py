d5f9512ddb24a52bd21f46a8d8ebc3fd5e80dfcd
lizagibradze <oreogibradze@gmail.com>
2024-11-14 20:01:50 +0400

    19commit

---
+++ day20/classwork/level20.py
@@ -0,0 +1,89 @@
+
+# დიღმის_მასივი = ["ნებისმიერი რამე", "არანაირი რამე", "თყ სუნთქავ არ კვდები", "ისე კარგად არხეინად იწოვდა"]
+       #  0123456
+# სახელი = "მისწერე აპო"
+
+# print(სახელი[0:3])
+#ნულიდან სამამდე გამოიტანს
+
+
+# print(სახელი[:4])
+
+
+
+
+# arr = ["davit", "gio", "davit", "gio","davit"]
+# counter = 0
+# is_count = "davit"
+# for elem in arr:
+#     if elem ==is_count:
+#         counter = counter + 1
+# print(counter)
+
+
+
+# c = ["a", "e", "i", "o"]
+# print(c[1:3])
+
+# print(c[2:])
+#bolo ori
+
+
+new_arr = [1,23,4,5,56,6,6,6,7,7,78,342,3423,4234212,1,312,31,341]
+
+new_arr[0] = "დავითი"
+
+
+for i in range(len(new_arr)):
+    if new_arr[i] % 2 != 0:
+        new_arr[i] = "კენტი"
+
+print(len(new_arr))
+print('----------------------------------------------')
+print(new_arr)
+
+
+
+
+#   if type(new_arr[i]) == int and new_arr[i] % 2 != 0:
+#         new_arr[i] = "კენტი"
+
+# print(new_arr)
+
+# new_arr = [1,23,3,4,5,56,6,6,6,7,7,78,2,342,3423,4234212,1,312,31,341]
+# new_arr[0] = "დავითი"
+# # lne() ---რომ გვითხრას რა სიგრძისაა ჩვენი სია ან თუმდაც ჩვენი string
+# print('----------------------------------------------')
+# # classworck  ჩვენი დავალება მდგომარეობს შემდეგში
+# # ვნახოთ რომელი ელემენტიც იქნება კენტი ის ელემენტები შეიცვალოს "კენტი"
+# for i in range(len(new_arr)):
+#     print(new_arr[i])
+
+
+
+# for i in range(4):
+    # print(new_arr[i])
+# for i in range(len(new_arr)):
+#     print(new_arr[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day20/homework/level20.py
@@ -0,0 +1,60 @@
+
+# 1
+
+# letters = ["a","b","c","a","i","c","e","o","u"]
+# vowel = ["a","e","i","o","u"]
+
+# vcount = 0
+
+
+# for letter in letters:
+#     if letter in vowel:
+#         vcount += 1
+
+# print(vcount)
+
+
+
+# 2
+
+# numbers = []
+
+# for i in range(1,101):
+#     if i % 5 == 0 and i % 3 == 0:
+#         numbers +=[i]
+
+# print(numbers)
+
+
+
+# 3
+
+
+# elements = ["a","1","o","b","2","3"]
+
+# combine = ""
+
+# for element in elements:
+#     combine += str(element)
+# print(combine)
+
+
+# 4
+
+
+
+# for i in range(6, 0, -2):
+#     print(" " * (6 - i) + "*" * i)
+
+# ?
+
+
+# 5
+
+age = int(input("how old are you "))
+
+if age == 12:
+    print("you are 12")
+
+if age > 12:
+    print("you are not 12")
