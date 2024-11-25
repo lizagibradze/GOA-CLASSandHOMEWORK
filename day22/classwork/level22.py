3350efe8a8da7009a54a8490ef2b239d60332e08
lizagibradze <oreogibradze@gmail.com>
2024-11-25 18:52:02 +0400

    23commit

---
+++ day22/classwork/level22.py
@@ -0,0 +1,118 @@
+
+# letter = ["a","b","c","a","y","d","d","f",]
+# vowel =["a","i","o","u","y",]
+# counter = 0
+
+# for char in letter:
+#     for vowel in vowel:
+#         if char == vowel:
+#             counter +=1
+
+# print(counter)
+
+
+# arr = [1,2,3,4] #mutable
+# arr[0] = "davit"
+# print(arr)
+
+# _str ="davit" #imutable
+# _str[0] = "d"
+# print(_str)
+
+
+
+# <--- minusebi 
+
+# c = ["a","o","u","i"]
+# print(c[-1])
+
+#meotxe
+
+
+
+# c = ["a","o","u","i"]
+
+# print(c[-3:-1])
+
+#2nd and 3rd
+
+
+
+
+#argument
+
+# print("group48")
+# range(1,30)
+# str("r")
+# type(1)
+
+#list, string , int , float,
+#list თავიანთი ფუნქციები გააჩნია
+#list---len---გვიბრუნდება სიგრძის ჩვენი სიისას
+#min / max
+
+
+
+# arr =[1,2,3,4,5,-1]
+# print(min(arr))
+# print(max(arr))
+
+
+
+
+# new_arr = ["davit"]
+# print("------------")
+# numb = 1+3
+# new_arr.append(numb)
+# new_arr.append("lomi")
+# new_arr.append("vefxvi")
+# new_arr.append(True)
+# print(new_arr)
+
+
+
+
+
+# list_of_odd = []
+# list_of_even = []
+
+# for num in range(1, 1001):
+#     if num % 2 == 0:
+#         list_of_even.append(num)
+#     else:
+#         list_of_odd.append(num)
+
+# print(list_of_even)
+
+# print(list_of_odd)
+
+
+
+
+# insert_arr = [1,2,3,4,5,6,"giorgi","kurosaki ichigo"]
+# insert_arr.insert(7, "daviti")
+# print(insert_arr)
+
+
+
+
+
+# letters = ["p","q","r","s","p","u",]
+# print(letters.index("r"))
+# print(letters.index("p"))
+# print(letters.index("q"))
+# print("-------------------")
+
+# romel indexze imyopeba 
+
+
+x = [2,4,2,6,2,7,2,9]
+print(x.count(2)) #3 (ramdeni oriania itvlis)
+
+x.remove(4) #otxians ashorebs
+print(x)
+
+x.reverse()
+print(x)
+
+
---
+++ day22/homework/level22.py
@@ -0,0 +1,46 @@
+#1
+
+list_of_names = ["giorgi","daviti","nika","giorgi","liza","daviti","daviti",]
+
+count_name = "daviti"
+counter = list_of_names.count(count_name)
+
+print(counter)
+
+#2
+
+numbers =  [1, 2, 3, 4, 5]
+
+numbers.reverse()
+print(numbers)
+
+
+
+#3
+
+list_of_numbers =  [1, 2, 3]
+
+replist = list_of_numbers * len(list_of_numbers)
+
+print(replist)
+
+
+#4
+
+insert_arr =["python","java","c++"]
+
+insert_arr.insert(1, "daviti")
+print(insert_arr)
+
+
+
+#5
+
+list = ["daviti","giorgi","nika","daviti",]
+count_daviti = list.count("daviti")
+list.remove("daviti")
+print(count_daviti, list)
+
+
+
+
