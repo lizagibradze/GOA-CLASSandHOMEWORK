43ee35f3cf1a56be4b906345be1d210347ccd0ea
lizagibradze <oreogibradze@gmail.com>
2024-11-30 21:14:03 +0400

    25commit

---
+++ day24/classwork/level24.py
@@ -0,0 +1,118 @@
+
+# nums = [4, 5, 6]
+# message = "davit" + str(10) + "wlis aris"
+
+
+#                                                  0      1
+# new_message = "daviti {0} wlis aris {1}".format(10, "daviti")
+
+# new_message = "daviti {y} wlis aris {x}".format(x=10, y="daviti")
+
+# print(message)
+# print(new_message)
+
+
+
+
+# nmessage = "me var {1}".format("gamarjoba","liza")
+
+# print(nmessage)
+
+
+
+
+
+# vasli = f"წითელი ვაშლი გაგორდა{50 + 50} ml"
+# print(vasli)
+
+
+
+# print("---------split---------")
+# #             am gamotovebuls wers arry is nairad  
+# name = "group 48 leader group"
+# split_name = name.split()
+# print(split_name)
+
+
+
+
+# print("---------split---------")      
+# name = "group 48 leader group"
+# split_name = name.split(" ")
+# print(split_name)
+
+# new_name = "group,48,leader,group,off..."
+# new_split_name = new_name.split("p") #sadac p ari mad daiwyebs gaxlichvas
+
+# print(new_split_name)
+
+
+
+
+# print("---------join---------") 
+# arr = ["group","48","leader,group", "off..."]
+# joined_arr = "!".join(arr)
+# print(joined_arr)
+
+
+
+
+
+
+# sentence = "იხვის ტოლმა არ არის ბატის ტიოლმა"
+# words = sentence.split()
+
+# new_sentence = "!".join(words) + "hi"
+
+# print(new_sentence)
+
+
+
+
+# joined_arr = "".join(split_name)
+# print(joined_arr)
+
+# name_1 = "davit grdzelishvili ","aris goas mentori".split(" ")
+# update_name_1 = "lomi".join(name_1)
+
+
+# update_name_1 = update_name_1.split(" ")
+# update_name_1 = ",".join(update_name_1)
+
+
+
+# print(name_1)
+# print(update_name_1)
+
+# print("-------------replace-----------")
+
+# example_1 = "a b c d a a a"
+# example_1_replace = example_1.replace("a","*")
+
+# print(example_1_replace)
+
+
+
+
+
+
+# example_1 = "aDAvitaGioaAloo"
+# example_1_replace = ",".join(example_1.replace("a", "*").split("*"))
+# print(example_1_replace)
+
+
+print("--------------ceases-----------")
+
+example_2 = "DaVIt grdzelishvili".upper() #adidebs yvelas
+example_3 = "DaVIt grdzelishvili".lower()# apataravebs yvelas
+example_4 = "DaVIt grdzelishvili".capitalize()#mxolod pirveli
+example_5 = "DaVIt grdzelishvili".title() #pirveli dasawyisi da meorec
+
+print(example_2)
+print(example_3)
+print(example_4)
+print(example_5)
+
+
+
+
---
+++ day24/homework/level24.py
@@ -0,0 +1,95 @@
+
+# 1
+
+# sentence = "goa group 48"
+# split = " "
+# result = []
+# word = ""
+
+# for char in sentence:
+#     if char == split:
+#         result.append(word)
+#         word = ""
+#     else:
+#         word += char
+
+
+
+
+# result.append(word)
+# print(result)
+
+
+
+
+
+# 2
+
+
+# arr = ["goa", "group", "48"]
+# space = " "
+
+# result = ""
+
+# for i in range(len(arr)):
+#     result += arr[i]
+#     if i < len(arr) - 1:
+#         result += space
+
+# print(result)
+
+
+
+
+
+
+
+
+# 3
+
+# sentence = "a b c d a a a"
+# ochar = "a"
+# nchar = "g"
+# result = ""
+
+# for char in sentence:
+#     if char == ochar:
+#         result += nchar
+#     else:
+#         result += char
+
+
+# print(result)
+
+# 4
+
+# num1 = 10
+# num2  = 5
+
+# add = num1 + num2
+# subtract = num1 - num2 
+# multiply = num1 * num2
+# divide = num1 / num2
+
+# print(add)
+# print(subtract)
+# print(multiply)
+# print(divide)
+
+# 5
+
+# words = []
+
+# inp = int(input("how many words do you want to input"))
+
+# for i in range(inp):
+#     word = input("enter word: ")
+#     words.append(word)
+
+# joinedwords = " ".join(words)
+
+# print(joinedwords)
+
+
+
+
