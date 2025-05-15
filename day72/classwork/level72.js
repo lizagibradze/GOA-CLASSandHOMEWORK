ddda164fba2bdadd876b424b9a7cf42d1cfedc1d
lizagibradze <oreogibradze@gmail.com>
2025-05-15 23:08:30 +0400

    commit

---
+++ day72/classwork/level72.js
@@ -0,0 +1,62 @@
+
+
+
+    let name = "liza"
+    name = "aeiou"
+    name = 12
+    name = [1, 2, 3]
+
+    console.log(name)
+
+ // way 2
+
+    //  ar viyenebt 
+    var lomi = "liz"
+    lomi = "lomlom"
+    var lomi = "liz"
+    console.log(lomi)
+    //  ar viyenebt 
+
+
+
+    // name bye = 2
+
+
+// way 3
+
+    const surname = "grdzelo"
+
+
+    console.log("hello world")
+    
+
+console.log("---------------------------")
+
+
+
+const body = document.getElementById("body")
+console.log(body)
+
+
+
+function bgChange(){
+    body.style.background = "green"
+
+}
+
+
+
+
+// function bgChange(){
+//     body.style.background = "skyblue"
+    
+//     body.style.padding = "0";
+//     body.style.boxSizing = "border-box";
+
+// }
+
+
+
+
+
+
---
+++ day72/homework/level72.js
@@ -0,0 +1,26 @@
+
+let age = 15
+
+
+console.log(age)
+
+
+
+
+let name = "liza"
+
+
+console.log(name)
+
+
+
+
+const body = document.getElementById("body")
+console.log(body)
+
+
+
+function back(){
+    body.style.background = "red"
+
+}
