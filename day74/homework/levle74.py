10c866c82cc6f7b757bee182c8a70f798252bcb0
lizagibradze <oreogibradze@gmail.com>
2025-05-26 23:06:05 +0400

    commit

---
+++ day74/homework/levle74.py
@@ -0,0 +1,14 @@
+
+
+
+num1 = int(input("შეიყვანე პირველი რიცხვი: "))
+num2 = int(input("შეიყვანე მეორე რიცხვი: "))
+
+
+if num1 > num2:
+    print("პირველი უფრო დიდია3")
+else:
+    if num1 < num2:
+        print("მეორე mუფრო დიდია")
+    else:
+        print("ტოლია")
